7b313b6985ff53a777674db8e6e04a4007d0b61d
loveonaj <54526281+loveonaj@users.noreply.github.com>
2019-12-03 00:14:34 -0500

    Documented Code

--- Bank.java
+++ Bank.java
@@ -1,17 +1,26 @@
-
-
 import java.util.*;
+
+/**
+   Defines information related to the process of creating bank accounts
+   that includes creating repository for accounts and tracking the
+   total number of accounts.
+*/
 
 //linkedlist and/or arraylist
 public class Bank extends Account {
-
+    // Repository for bank accounts
     private Account[] bankAccs = new Account[10];
+    // Number of bank account limits
     private int maxacct = 10;
+    // Tracks number of total bank accounts
     private static int acctcounter = 0;
 
-
+    /**
+      Constructor for Bank class that instantiates Bank object.
+    */
     private Bank() {
         //initialize data structures
+        // Assigns and places new account within repository
         bankAccs[acctcounter] = new Account();
     }
 
