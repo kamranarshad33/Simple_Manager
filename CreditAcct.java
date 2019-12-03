baec9feb612ac2adbbded4a8b7b43be9842386ff
loveonaj <54526281+loveonaj@users.noreply.github.com>
2019-12-03 00:01:57 -0500

    Documented code

--- CreditAcct.java
+++ CreditAcct.java
@@ -1,7 +1,19 @@
+/**
+   The CheckinAcc class is defined as a credit account type
+   inherited from the parent Account class.
+*/
 public class CreditAcct extends Account{
 
+    // Type of account
     private String Accttype;
 
+    /**
+      Constructor for CreditAcct class that instantiates a CreditAcct object.
+      @param accttype type of account
+      @param bankname name of bank
+      @param routing routing number of checking account
+      @param balance checking account balance
+    */
     public CreditAcct(String accttype, String bankname, String routing, double balance){
         super(accttype,bankname,routing,balance);
         this.Accttype = "Credit Account" ;
