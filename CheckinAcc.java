973faf5a4e98a31db7c8a323235b306afe644461
loveonaj <54526281+loveonaj@users.noreply.github.com>
2019-12-03 00:00:38 -0500

    Documented Code

--- CheckinAcc.java
+++ CheckinAcc.java
@@ -1,9 +1,19 @@
-
+/**
+   The CheckinAcc class is defined as a checking account type
+   inherited from the parent Account class.
+*/
 public class CheckinAcc extends Account{
 
-
+    // Type of account
     private String Accttype;
-
+    
+    /**
+      Constructor for CheckinAcc class that instantiates a CheckinAcc object.
+      @param accttype type of account
+      @param bankname name of bank
+      @param routing routing number of checking account
+      @param balance checking account balance
+    */
     public CheckinAcc(String accttype, String bankname, String routing, double balance){
         super(accttype,bankname,routing,balance);
         this.Accttype= "Checking Account" ;
